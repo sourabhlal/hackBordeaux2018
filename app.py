cada3af6e4e062aebead2745a4bc87b37ff3132f
Sourabh <sourabhlal@gmail.com>
2018-03-24 12:51:38 -0700

    updating for heroku

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, request, abort
-
+from datetime import datetime
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
+
+@app.route('/hello', methods=['GET'])
 def hello():
     return 'hello world! we are at hack bordeaux', 200
 
@@ -13,7 +24,6 @@ def webhook():
         return '', 200
     else:
         abort(400)
-
-
+        
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, use_reloader=True)
